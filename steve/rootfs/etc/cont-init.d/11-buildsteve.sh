e5a0d4e9b93f21c97b9e91194af6270084aba65a
Erik Hilton <erikhilton73@gmail.com>
2023-06-05 16:10:22 +0200

    Remove Maven memory limit

--- steve/rootfs/etc/cont-init.d/11-buildsteve.sh
+++ steve/rootfs/etc/cont-init.d/11-buildsteve.sh
@@ -7,7 +7,7 @@
 if ! bashio::fs.directory_exists '/data/target'; then
     bashio::log.info "Starting SteVe initial build...."
     cd /usr/src/steve || exit
-    MAVEN_OPTS="-Xmx100m" mvn package
+    mvn package
     cp -f /usr/src/steve/pom.xml /data/
     cp -R -f /usr/src/steve/target/ /data/
 fi
