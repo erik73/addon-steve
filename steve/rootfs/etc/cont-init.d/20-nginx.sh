059672484753b7e894fc45a766f3c1ed10c2696c
Erik Hilton <erikhilton73@gmail.com>
2026-01-07 12:45:21 +0100

    Update 20-nginx.sh

--- steve/rootfs/etc/cont-init.d/20-nginx.sh
+++ steve/rootfs/etc/cont-init.d/20-nginx.sh
@@ -7,6 +7,7 @@
 # Generate Ingress ingress configuration
 bashio::var.json \
     interface "$(bashio::addon.ip_address)" \
+    port "^$(bashio::addon.ingress_port)" \
     | tempio \
         -template /etc/nginx/templates/ingress.gtpl \
         -out /etc/nginx/servers/ingress.conf
